b9d073c460e3e4db1ae46806b661e6da6e664260
bbeaarr <bortsimon@gmail.com>
2016-04-05 13:40:00 +0200

    UrlaubController erstellt

---
+++ app/Http/controllers/UrlaubController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class UrlaubController extends Controller
+{
+  /**
+  *
+  */
+  public function index()
+  {
+    //
+  }
+  /**
+  * Neuer Eintrag erstellen.
+  *
+  */
+  public function create()
+  {
+    //
+  }
+  /**
+  * Speicher einen neu erstellten Eintrag.
+  *
+  */
+  public function store()
+  {
+    //
+  }
+  /**
+  * Einträge anzeigen.
+  *
+  */
+  public function show($id)
+  {
+    //
+  }
+
+  /**
+  * Eintrag editieren.
+  *
+  */
+  public function edit($id)
+  {
+    //
+  }
+  /**
+  * Eintrag updaten.
+  *
+  */
+  public function update($id)
+  {
+    //
+  }
+  /**
+  * Eintrag löschen.
+  *
+  */
+  public function destroy($id)
+  {
+    //
+  }
+}
+}
